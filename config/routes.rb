19eb5824e1cd727b1eeb84e2eda36da21f741988
Richard-Frost <frost.rchrd@gmail.com>
2020-03-03 13:14:53 -0500

    added static controller, welcome page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :children
   resources :families
   resources :users
+  root to: 'static#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
