b5d57c5a966cca762813dc4cfb81a18226c6e537
Richard-Frost <frost.rchrd@gmail.com>
2020-04-01 11:09:37 -0400

    user edit function, home page, routes, application helpers)

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -3,5 +3,9 @@ class ChildrenController < ApplicationController
 	def new 
 	  @child - Child.new
 	end
+
+	def show
+	 @child = Child.find_by(params[:id])
+	end
 	
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,25 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find_by(id: session[:user_id])
 	end
+
+	def home
+	 @family = my_family
+	end
+
+	def edit
+	  @user = User.find_by(id: session[:user_id])
+	end
+
+	def update
+    @user = User.find_by(id: session[:user_id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
 end
